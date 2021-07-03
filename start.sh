e42394d0b1d2cc413c673e6918376d529c74ee0f
Jacob Ochoa <jacob.ochoa@gmail.com>
2021-07-02 18:16:30 -0700

    fix start

--- start.sh
+++ start.sh
@@ -9,7 +9,7 @@ docker run \
 	-p 19132:19132/udp \
 	-p 24454:24454/udp \
 	-p 25575:25575/tcp \
-	-e RCON_PASSWORD=$RCON_PASSWORD \
+	-e RCON_PASSWORD="$RCON_PASSWORD" \
 	-e LEVEL_SEED="-7672362719859801980" \
 	--name borkcraft \
 	--restart unless-stopped \
